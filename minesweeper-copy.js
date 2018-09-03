2561586619ccdb47443b02893debb0ac83e2915a
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-09-03 16:08:39 -0400

    created a function printBoard

--- minesweeper-copy.js
+++ minesweeper-copy.js
@@ -1,14 +1,26 @@
-const blankLine = '  |   |  ';
-console.log(`This is what an empty board would look like:`);
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '], 
+  [' ', ' ', ' ']  
+];
 
-const guessLine = '1 |   |  ';
+const bombBoard = [];
 
-const bombLine = '  | B |  ';
-console.log(`This is what a board with a guess and a bomb on it would look like:
-`);
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const playerBoard = [];
+
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+printBoard(board);
+
+board[1][2] = 1;
+
+printBoard(board);
+
+board[1][1] = 'B';
+
+printBoard(board);
