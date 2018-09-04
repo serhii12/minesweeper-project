5ae0755932d64cf955a36ff30216f556e8a4038f
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-09-04 13:06:07 -0400

    added functionality for a user to flip tile and reveal bombs around

--- minesweeper-copy.js
+++ minesweeper-copy.js
@@ -1,8 +1,8 @@
 /*
 The function should:
 
-Add an empty space (' ') to each column in every row
-Add each row to to a larger game board, thereby constructing the player's board
+1) Add an empty space (' ') to each column in every row
+2) Add each row to to a larger game board, thereby constructing the player's board
 */
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   const board = [];
@@ -15,14 +15,15 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   }
   return board; // Return the board array
 };
+
+
+
 /*
 The function should:
 
-Create the game board of the specified size
+1) Create the game board of the specified size
 
-Add bombs to random squares on the game board
-
-For example, generateBombBoard(5, 9, 14) would result in a 5 x 9 game board (45 total squares) with 14 bombs placed randomly on the board.
+2) Add bombs to random squares on the game board
 */
 const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   const board = [];
@@ -41,22 +42,96 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 
     if (board[randomRowIndex][randomColumnIndex] !== 'B') { // Check if there is bomb alredy if not place one
       board[randomRowIndex][randomColumnIndex] = 'B';
-    } else {
-      const newRandomRowIndex = Math.floor(Math.random() * numberOfRows); // Random row to place the bomb
-      const newRandomColumnIndex = Math.floor(Math.random() * numberOfColumns); // Random column to place the bomb
-      board[newRandomRowIndex][newRandomColumnIndex] = 'B';
-    }
-
-    numberOfBombsPlaced++;
+      numberOfBombsPlaced++;
+    } 
   }
   return board; // Return the board array
 };
 
+
+
+/*
+1) The function will determine the size of the game board
+
+2) The function will use the location of the flipped tile
+
+3) Using an array index offset system (more on this later), the function will check all adjacent tiles for bombs
+*/
+const getNumberOfNeighborBombs = (bombBoard,rowIndex,columnIndex) => {
+  const neighborOffsets = [
+    [-1,-1],
+    [-1,0],
+    [-1,1],
+    [0,-1],
+    [0,1],
+    [1,-1],
+    [1,0],
+    [1,1]
+  ];//8 possible offset combintations of neighboring tiles.
+
+  const numberOfRows = bombBoard.length; // Check how many rows we have in bombBoard
+
+  const numberOfColumns = bombBoard[0].length;// Check how many colums we have in bombBoard
+
+  let numberOfBombs = 0; // See how many bombs are around
+
+  neighborOffsets.forEach(offset => {
+   const neighborRowIndex = rowIndex + offset[0];
+   const neighborColumnIndex = columnIndex + offset[1];
+
+   if (neighborRowIndex >= 0 && 
+    neighborRowIndex < numberOfRows && 
+    neighborColumnIndex >= 0 && 
+    neighborColumnIndex < numberOfColumns) {
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+      numberOfBombs++; // Check around flipTile and get total number of bombs
+      }
+    }
+  });
+  return numberOfBombs; // Let player know how many bombs are around;
+};
+
+
+
+/* 
+The function should explicitly check for two things:
+
+1) If the specified tile has already been flipped
+
+2) If the specified tile has a bomb in it
+
+3) Otherwise, that tile should be updated with the number of neighboring bombs.
+
+*/
+
+const flipTile = (playerBoard,bombBoard,rowIndex,columnIndex) => {
+  if (playerBoard[rowIndex][columnIndex] !== ' ') { // The tile is not empty (already been flipped)
+    console.log(`This tile has already been flipped`);
+    return;
+  } else if (bombBoard[rowIndex][columnIndex] === 'B') { // There is a bomb at that tile
+    playerBoard[rowIndex][columnIndex] = 'B';
+  } else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard,rowIndex,columnIndex);
+  }
+};
+
+
+// Function to print board
 const printBoard = (board) => {
   console.log(board.map(row => row.join(' | ')).join('\n')); // This will join together the array of rows with new lines, placing each row on its own line when printed.
 };
 
-console.log('Player Board:');
-printBoard(generatePlayerBoard(3,4));
+let playerBoard = generatePlayerBoard(3, 3);
+let bombBoard = generateBombBoard(3, 3, 3);
+
+console.log('Player Board:')
+printBoard(playerBoard);
+
 console.log('Bomb Board:');
-printBoard(generateBombBoard(3,4,5));
+printBoard(bombBoard);
+
+flipTile(playerBoard, bombBoard, 0, 0); // Flip different tiles based on bombBoard to see if neighbors work.
+
+console.log('Updated Player Board:');
+// Updated Player Board
+printBoard(playerBoard);
